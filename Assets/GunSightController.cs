f2736eb4fcac86690d749432be2cd09d0894e3be
Keir Smith <Keir Smith>
2017-04-16 11:24:56 +0100

    More realistic gun sight

---
+++ Assets/GunSightController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSightController : MonoBehaviour {
+    public Transform sightCamTrans;
+    public Transform sight;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = new Vector3(sightCamTrans.localPosition.z, sightCamTrans.localPosition.y, sightCamTrans.localPosition.x) * 10;
+        transform.localEulerAngles = new Vector3(sightCamTrans.localRotation.z, sightCamTrans.localRotation.y, sightCamTrans.localRotation.x);
+        /*if (sightCamTrans.localPosition.x > 0)
+        {
+            transform.localPosition = new Vector3(0, transform.localPosition.y, transform.localPosition.z);
+        }*/
+        //sight.eulerAngles = Vector3.zero;
+	}
+}
